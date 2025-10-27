4c53304f4f476759184ec97a27eed13a0c25ba8a
Daria <kordesovad@gmail.com>
2025-10-27 10:41:00 +0100

    split one css file into divide files

--- src/components/Tank.jsx
+++ src/components/Tank.jsx
@@ -1,12 +1,9 @@
 import ExpCalculator from "./ExpCalculator"
 
-function handleHover () {
-    console.log("hey");
-}
 
 function Tank( {name} ) {
     return (
-        <div className="tank_wrapper" onMouseEnter={handleHover} data-tank={name}>
+        <div className="tank_wrapper" data-tank={name}>
             <div className="tank">
                 <img src="./src/assets/tank.png" alt="tank image" />
                 <h6>{name}</h6>
